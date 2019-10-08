9487bb90f76bfe5d842a01a50566829bbc98537b
james <james@bluebaron.ca>
2019-10-08 11:04:35 -0400

    Removed sensitive credentials.

--- WebApplication/Model/ApplicationDbContext.cs
+++ WebApplication/Model/ApplicationDbContext.cs
@@ -14,6 +14,15 @@ namespace WebApplication.Model
             : base(options)
         {
         }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("");
+            }
+        }
 
         public virtual DbSet<Odometer> Odometer { get; set; }
         public virtual DbSet<Vehicle> Vehicle { get; set; }
